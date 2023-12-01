a36d32d001d908db21491bd403326790ca6179d7
Nora_de_Greeuw <37214@ma-web.nl>
2023-12-01 09:55:30 +0100

    les 2

--- 01A/app.js
+++ 01A/app.js
@@ -1,7 +1,21 @@
 class app {
 
+    
     runApplication() {
+        let appName = "SchoolOpdracht"
+        let versionApp = 0.11
+        let versionAppDate = "Last edited: 12/09/2022 20:39 (European notation)"
+        let darkMode = true
+        let propertyArray = [appName, versionApp, versionAppDate, darkMode];
         console.log("Hello world")
+        checkProperties(propertyArray);
+    }
+    
+}
+
+function checkProperties(propertyArray) {
+    for (let i = 0; i < propertyArray.length; i++) {
+        console.log(propertyArray[i]);
     }
 }
 
